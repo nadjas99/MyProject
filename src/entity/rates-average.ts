b68808d1d6b4a8523cf94a3fa6f53c11fbb02e67
Nadja Stojancevic <stojancevicnadja@gmail.com>
2022-06-09 20:04:22 +0200

    averages,

---
+++ src/entity/rates-average.ts
@@ -0,0 +1,3 @@
+export interface average {
+  [curencie: string]: number;
+}
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,17 @@
 import "reflect-metadata";
 import { AppDataSource } from "./data-source";
 import { Country } from "./entity/Country";
+import { average } from "./entity/rates-average";
 const axios = require("axios");
 var countryData: Country[] = [];
 AppDataSource.initialize();
+var ratesDates = [];
+var avg: average = {};
+var avgList: average[] = [];
 
 export async function getCountries() {
   const { data } = await axios.get("https://restcountries.com/v3.1/all");
-  const countryRepository = data.forEach(async (country) => {
+  data.forEach(async (country) => {
     if (
       country.cca3 === "AUS" ||
       country.cca3 === "CHN" ||
@@ -34,12 +38,32 @@ export async function getRates() {
   const { data } = await axios.get(
     "https://api.apilayer.com/fixer/timeseries?start_date=2022-05-08&end_date=2022-06-08&symbols=BRL%2C%20USD%2C%20GBP%2C%20AUD%2C%20CNY",
     {
-      headers: { apikey: "cqMNFLecUQ51HaY5hNzJqhhoAWE2WC6E" },
+      headers: { apikey: "5D152LR4teyyVwgnicyK3XUz7Owf08BK" },
     }
   );
   const { rates } = data;
-  console.log(rates);
+  var i = 0;
+  ratesDates = Object.values(rates);
+  avg.BRL = 0;
+  avg.USD = 0;
+  avg.GBP = 0;
+  avg.AUD = 0;
+  avg.CNY = 0;
+  ratesDates.forEach((element) => {
+    i++;
+    avg.BRL += element.BRL;
+    avg.USD += element.USD;
+    avg.GBP += element.GBP;
+    avg.AUD += element.AUD;
+    avg.CNY += element.CNY;
+  });
+  avg.BRL = avg.BRL / 30;
+  avg.USD = avg.USD / 30;
+  avg.GBP = avg.GBP / 30;
+  avg.AUD = avg.AUD / 30;
+  avg.CNY = avg.CNY / 30;
+  console.log(avg);
 }
 
-getCountries();
+//getCountries();
 getRates();
